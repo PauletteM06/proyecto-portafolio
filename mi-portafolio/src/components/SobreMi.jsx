87959bcf52fabcf760f9fedfd8ef1d6dc371c22e
Scarlett Marquez <PauletteM06@users.noreply.github.com>
2025-10-26 18:27:36 -0300

    Componentes Header y Sobre mi creados

---
+++ mi-portafolio/src/components/SobreMi.jsx
@@ -0,0 +1,15 @@
+
+const SobreMi = () => {
+  return (
+    <>
+    <div className='sobre_mi_container'>
+    <h2 className="sobre_mi_title">Sobre Mi</h2>
+    <p>Analista Programador Titulada.</p>
+    <p className="sobre_mi_description">Me gusta aprender, mejorar y construir proyectos que me reten cada día.</p>
+    <p className="sobre_mi_description">Prefiero mostrar lo que sé hacer, así que dejaré que mis proyectos hablen por mí.</p>
+    </div>
+    </>
+  )
+}
+
+export default SobreMi
